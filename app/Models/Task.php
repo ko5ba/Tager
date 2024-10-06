619410d8ab86c2943969c7637164ff877870a8f0
ko5ba <104355972+ko5ba@users.noreply.github.com>
2024-10-06 18:34:16 +0300

    Initial commit

---
+++ app/Models/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'tasks';
+    protected $fillable = [];
+
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
